def092aa4d32ab6a8091a4cf0959e7f32bdd4c10
plei0016 <42801569+plei0016@users.noreply.github.com>
2018-09-21 16:39:11 +0200

    Restructured files and added mobile settings

---
+++ assets/javascript/general.ts
@@ -0,0 +1,9 @@
+function menu() {
+    //console.log("Hello!");
+    if (document.getElementById("dropdown").style.display == "initial") {
+        document.getElementById("dropdown").style.display = "none";
+    }
+    else {
+    document.getElementById("dropdown").style.display = "initial";
+    }
+}
